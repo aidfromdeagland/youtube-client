a5b57050f71e644dd994c73950b3fcd46864c19f
aidfromdeagland <aidfromdeagland@gmail.com>
2020-10-01 21:07:13 +0300

    feat: add interfaces

--- src/app/models/search-item.ts
+++ src/app/models/search-item.ts
@@ -1,2 +1,41 @@
 export interface SearchItem {
+  kind: string;
+  etag: string;
+  id: string;
+  snippet: Snippet;
+  statistics: Statistics;
+}
+
+interface Snippet {
+  publishedAt: Date | string;
+  channelId: string;
+  title: string;
+  description: string;
+  thumbnails: {
+    default: Thumbnail,
+    medium: Thumbnail,
+    high: Thumbnail,
+    standard: Thumbnail,
+    maxres: Thumbnail
+  };
+  channelTitle: string;
+  tags: string[];
+  categoryId: string;
+  liveBroadcastContent: string;
+  localized: { title: string, description: string };
+  defaultAudioLanguage: string;
+}
+
+interface Thumbnail {
+  url: string;
+  width: number;
+  height: number;
+}
+
+interface Statistics {
+  viewCount: string;
+  likeCount: string;
+  dislikeCount: string;
+  favoriteCount: string;
+  commentCount: string;
 }
--- src/app/models/search-response.ts
+++ src/app/models/search-response.ts
@@ -1,2 +1,11 @@
+import {SearchItem} from './search-item';
+
 export interface SearchResponse {
+  kind: string;
+  etag: string;
+  pageInfo: {
+    totalResults: number,
+    resultsPerPage: number
+  };
+  items: SearchItem[];
 }
